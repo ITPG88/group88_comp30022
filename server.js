7c8c0385aa0f7df304284d53f7ac8d7337d1d9ad
Sebastian <sdaveygargan@student.unimelb.edu.au>
2022-08-19 23:14:30 +1000

    commented server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
 //npm i express morgan nodemon body-parser dotenv mongoose to install dependencies
@@ -15,6 +16,14 @@ app.use(morgan("tiny"));
 // Using body-parser
 app.use(bodyParser.urlencoded({extended:true}));
 
+// View engine
+//app.set("view engine", "html"); we're using html for that right?
+//app.set("views", path.resolve(__dirname, "views"));
+
+// assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
 
 app.get('/', (req, res) => {
     res.send("Crud app");
