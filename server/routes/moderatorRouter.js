8a81e0ec057ed24b23af80b4843a97353ee71cd2
qllyu <qllyu@student.unimelb.edu.au>
2022-09-15 23:40:02 +1000

    separate the moderators' and students' router

---
+++ server/routes/moderatorRouter.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const passport = require('passport')
+
+
+// create Router object
+const moderatorRouter = express.Router();
+// import controller functions
+const loginController = require("../controller/loginController");
+
+// Authentication middleware
+const isAuthenticated = (req, res, next) => {
+    // If user is not authenticated via passport, redirect to login page
+    if (!req.isAuthenticated()) {
+        return res.redirect('/moderator/login')
+    }
+    if (req.user.user_type != 'moderator'){
+        req.logout()
+        return res.redirect('/moderator/login')
+    }
+    // Otherwise, proceed to next middleware function
+    return next()
+}
+
+// Login page (with failure message displayed upon login failure)
+moderatorRouter.get('/login', (req, res) => {
+    res.render('ModeratorLogin', { flash: req.flash('error'), title: 'Login' })
+})
+
+// Handle login
+moderatorRouter.post('/login',
+    passport.authenticate('local', {
+        successRedirect: './dashboard', failureRedirect: './login', failureFlash: true
+    })
+)
+// Handle logout
+moderatorRouter.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/doctor/login')
+})
+
+// Some initial routing
+moderatorRouter.get("/", loginController.landing);
+moderatorRouter.get("/signup", loginController.signup);
+module.exports = router;
--- server/routes/studentRouter.js
+++ server/routes/studentRouter.js
@@ -1,6 +1,36 @@
-const express = require("express");
+const express = require('express')
+const passport = require('passport')
+
 
 // create Router object
-const router = express.Router();
+const studentRouter = express.Router();
+// import controller functions
+const loginController = require("../controller/loginController");
 
-//All HTML requests in here should be within /students
+// Authentication middleware
+const isAuthenticated = (req, res, next) => {
+    // If user is not authenticated via passport, redirect to login page
+    if (!req.isAuthenticated()) {
+        return res.redirect('/student/login')
+    }
+    if (req.user.user_type != 'student'){
+        req.logout()
+        return res.redirect('/student/login')
+    }
+    // Otherwise, proceed to next middleware function
+    return next()
+}
+
+// Login page (with failure message displayed upon login failure)
+// router.get("/login", (req, res) => {
+//   res.render("Login", { flash: req.flash("error"), title: "Login" });
+// });
+
+
+// Some initial routing
+studentRouter.get("/", loginController.landing);
+studentRouter.get("/signup", loginController.signup);
+module.exports = router;
+
+// add a route to handle the GET request for student homepage
+studentRouter.get("/:student_id", studentController.getCurrentStudent);
