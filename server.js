653dc6ff5a6fe6ef93ba13758f64dd3d4fbe4ade
qllyu <qllyu@student.unimelb.edu.au>
2022-09-16 15:48:06 +1000

    add express-session so that the server can handle the get message correctly

--- server.js
+++ server.js
@@ -4,17 +4,37 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const session = require('express-session')
 const path = require("path");
 const app = express();
-
-// Initialise Passport.js
-const passport = require('./passport')
-app.use(passport.authenticate('session'))
-
 
 //Create your own config.env file
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 8080;
+
+// Track authenticated users through login sessions
+app.use(
+    session({
+        // The secret used to sign session cookies (ADD ENV VAR)
+        secret: process.env.SESSION_SECRET || 'keyboard cat',
+        name: 'demo', // The cookie name (CHANGE THIS)
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            sameSite: 'strict',
+            httpOnly: true,
+            secure: app.get('env') === 'production'
+        },
+    })
+)
+
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1); // Trust first proxy
+}
+
+// Initialise Passport.js
+const passport = require('./passport')
+app.use(passport.authenticate('session'))
 
 // log any requests
 app.use(morgan("tiny"));
@@ -28,6 +48,8 @@ app.engine("html", require("ejs").renderFile);
 
 app.set("views", path.resolve(__dirname, "views"));
 app.use(express.static(__dirname + "/"));
+
+
 // assets
 // app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 // app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
