53b5f0c75ec7a276620f9c7ba3dda155376bf0af
Sebastian <sdaveygargan@student.unimelb.edu.au>
2022-09-25 22:34:06 +1000

    moving test logic into correct routes

---
+++ server/controller2/subjectController.js
@@ -0,0 +1,20 @@
+const Review = require("../model/review");
+const Subject = require("../model/subject");
+const User = require("../model/user").User;
+const Student = require("../model/user").Student;
+const mongoose = require("mongoose");
+
+
+exports.loadSubjectPage = async (req, res) => {
+
+}
+
+exports.loadSingleReview = async (req, res) => {
+     const review = await Review.findById(req.params.id);
+
+     if (!review){
+
+     }
+
+     res.render("./student/view_review", {review: review, subjectCode: req.params.subject});
+}
--- server/routes2/subject.js
+++ server/routes2/subject.js
@@ -1,9 +1,17 @@
 const auth = require("../services/auth");
 const reviewController = require("../controller2/reviewController");
+const subjectController = require("../controller2/subjectController");
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
 
+router.get('/:subjectCode', subjectController.loadSubjectPage);
+
+router.get('/:subjectCode/review/:id', subjectController.loadSingleReview);
+
+router.get('/', (req, res) =>{
+    res.status(404);
+});
 
 module.exports = router;
