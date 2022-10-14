bb3f70b5e5ba898193b9b73e6bcd16613359d4bb
qllyu <qllyu@student.unimelb.edu.au>
2022-10-14 15:13:30 +1100

    use Heroku CLI to set MONGO_URL for our Heroku app

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () =>{
     try{
         //Attempt to connect to database
-        const con = await mongoose.connect(process.env.MONGO_URI, {
+        const con = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
