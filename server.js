cec397c5a8352ec39a91971f184c77a4d2ca673b
sdaveygargan <81617067+sdaveygargan@users.noreply.github.com>
2022-10-14 15:48:59 +1100

    Server 2 is now server.js and is the only server

--- server.js
+++ server.js
@@ -1,36 +1,48 @@
-//npm i express morgan nodemon body-parser dotenv mongoose axios express-validator to install dependencies
-//const passport = require("passport");
+// nodemon server2.js
+const passport = require("passport");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const methodOverride = require('method-override')
 const bodyParser = require("body-parser");
 const flash = require("express-flash");
 const session = require("express-session");
-const methodOverride = require('method-override')
 const path = require("path");
 const app = express();
 const connectDB = require("./server/database/connection");
+const {Review} = require("./server/model/review");
 
-//Create your own config.env file
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 8080;
 
+const cors = require("cors");
+const ejs = require("ejs");
+const SGmail = require("@sendgrid/mail");
+const apiKey = `${process.env.SENDGRID_API_KEY}`;
+console.log("SendGrid key ", apiKey);
 
-// Passport include
-//require("./server/services/passport.js")(passport);
-
-// Get DB connection
+require("./server/services/passport")(passport);
 connectDB();
 
-// Flash messages for failed logins, and (possibly) other success/error messages
 app.use(flash());
 
-// Track authenticated users through login sessions
+// Global vars
+/*
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.local.error_msg = req.flash('error_msg');
+    next();
+})*/
+
+//to enable cors
+app.use(cors());
+
+// Session
 app.use(
   session({
     // The secret used to sign session cookies (ADD ENV VAR)
     secret: process.env.SESSION_SECRET || "keyboard cat",
-    name: "demo", // The cookie name (CHANGE THIS)
+    name: "user", // The cookie name (CHANGE THIS)
     saveUninitialized: false,
     resave: false,
     cookie: {
@@ -40,85 +52,94 @@ app.use(
     },
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
-if (app.get("env") === "production") {
-  app.set("trust proxy", 1); // Trust first proxy
-}
-
-
-// Initialise Passport.js
-const passport = require("./passport");
-app.use(passport.authenticate("session"));
-
-// log any requests
-app.use(morgan("tiny"));
-
-// Using body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
+// Express session and flash
 app.use(express.json());
 
 // View engine
-app.set("view engine", "html");
-app.engine("html", require("ejs").renderFile);
-
+app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 app.use(express.static(__dirname + "/"));
-
-
-//ejs engine
-
-app.set('view engine', 'ejs')
+app.use(methodOverride('_method'));
 app.use('/static', express.static( "static" ))
 app.use('/static/css', express.static( "styles" ))
-app.use(express.urlencoded({extended : false}))
-app.use(methodOverride('_method'));
+
+// Bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Morgan for development
+app.use(morgan("tiny"));
+
+// Routes
+app.use("/", require("./server/routes2/index"));
+app.use("/settings", require("./server/routes2/settings"));
+app.use("/subject", require("./server/routes2/subject"));
+
+SGmail.setApiKey(process.env.SendGrid_Key);
+
+//routes which handles the requests
+app.get("/hello", (req, res, next) => {
+    let emailTemplate;
+    let capitalizedFirstName = "John";
+    let userEmail = "1105138402@qq.com";
+    ejs
+      .renderFile(path.join(__dirname, "views/welcome-mail.ejs"), {
+        user_firstname: capitalizedFirstName,
+        confirm_link: "http://www.8link.in/confirm=" + userEmail
+      })
+      .then(result => {
+        emailTemplate = result;
+  
+        const message = {
+          to: userEmail,
+          from: { email: "welcome@8link.in", name: "8link" },
+          subject: "Welcome link",
+          html: emailTemplate
+        };
+  
+        return SGmail.send(message)
+          .then(sent => {
+            // Awesome Logic to check if mail was sent
+            res.status(200).json({
+              message: "Welcome mail was sent"
+            });
+          })
+          .catch(err => {
+            console.log("Error sending mail", err);
+            res.status(400).json({
+              message: "Welcome mail was not sent",
+              error: err
+            });
+          });
+  
+        //res.send(emailTemplate);
+      })
+      .catch(err => {
+        res.status(400).json({
+          message: "Error Rendering emailTemplate",
+          error: err
+        });
+      });
+  });
+  
 
 
-
-//ends here
-
-//Passport middleware
-//app.use(passport.initialize);
-//app.use(passport.session);
-
-
-// assets
-app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
-app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
-app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
-
-
-//All root routes directed by router
-/*app.use("/", require("./server/routes/index.js"));
-app.use("/admin", require("./server/routes/admin.js"));
-app.use("/subject", require("./server/routes/subject.js"));
-app.use("/settings", require("./server/routes/settings.js"));*/
-
-app.get('/', (req,res) =>{
-  res.render('index')
-})
-app.use('/student', require('./server/route_test/student'))
-
-app.use('/moderator', require('./server/route_test/moderator'))
-// link to our router
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.all("*", (req, res) => {
+  res.redirect("/error404");
+});
+// Error 404 not found
+app.all("*", (req, res) => {
+  // 'default' route to catch user errors
+  res
+    .status(404)
+    .render("error", { errorCode: "404", message: "That route is invalid." });
+  //res.send('error')
 });
 
 
-/*
-const mongoose = require("mongoose");
-testing stuff
-const userID = mongoose.Types.ObjectId('632928b70eaf462946524199');
-const testUpdateReq = {
-    body: {
-        email: "newEmail@yahoo.com.au"
-    }
-}
-testUpdateReq.body.userID = userID;
-console.log(testUpdateReq);
-require("./server/controller/studentController").updateStudentUser(testUpdateReq)*/
-
-
+// Listen on port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
